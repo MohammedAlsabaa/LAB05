abbaed7b3ef1f73aa40228a87fc30eb656117734
MohammedAlsabaa <mys-200@hotmail.com>
2022-10-07 19:38:45 +0300

    sum

--- Sorter.java
+++ Sorter.java
@@ -1,11 +1,13 @@
 public class Sorter{
 
     public static void main(String[] args) {
-    
         System.out.println("Hello!!!!");
-        System.out.println("We are the best");
-        System.out.println("How to use commit");
-        System.out.println("last try");
+        int x = 5;
+        int y = 7;
+        int  sum = x + y;
+        System.out.println(sum);
+        
+  
 
     
     }
