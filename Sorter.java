e4cf77948df5c0a816b24df24e30e1e2a8b2bf42
Mohammed Alzaher <m1alzaher@gmail.com>
2022-10-07 20:51:43 +0300

    B

--- Sorter.java
+++ Sorter.java
@@ -30,4 +30,34 @@ public class Sorter{
               array[min_idx] = temp;
             }
           }
-    }
+
+
+          void insertionSort(int array[]) {
+            int size = array.length;
+        
+            for (int step = 1; step < size; step++) {
+              int key = array[step];
+              int j = step - 1;
+        
+              // Compare key with each element on the left of it until an element smaller than
+              // it is found.
+              // For descending order, change key<array[j] to key>array[j].
+              while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+              }
+        
+              // Place key at after the element just smaller than it.
+              array[j + 1] = key;
+            }
+          }
+          public static void sort2(String args[]) {
+            int[] data = { 9, 5, 1, 4, 3 };
+            Sorter is = new Sorter();
+            is.insertionSort(data);
+            System.out.println("Sorted Array in Ascending Order: ");
+            System.out.println(Arrays.toString(data));
+          }
+
+
+        }
