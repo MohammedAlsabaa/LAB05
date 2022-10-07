dd72057b5fde81e71cd5f6aa64876ecea00f18cc
MohammedAlsabaa <mys-200@hotmail.com>
2022-10-07 19:54:07 +0300

    selectionSort

--- Sorter.java
+++ Sorter.java
@@ -1,17 +1,33 @@
+import java.util.Arrays;
+
 public class Sorter{
 
     public static void main(String[] args) {
-        System.out.println("Hello!!!!");
-        int x = 5;
-        int y = 7;
-        int  sum = x + y;
-        System.out.println(sum);
+        int[] data = { 20, 12, 10, 15, 2 };
+        Sorter ss = new Sorter();
+        ss.selectionSort(data);
+        System.out.println("Sorted Array in Ascending Order: ");
+        System.out.println(Arrays.toString(data));
+        }
+        void selectionSort(int array[]) {
+            int size = array.length;
         
-  
-
-    
-    }
-    
-    
-    
+            for (int step = 0; step < size - 1; step++) {
+              int min_idx = step;
+        
+              for (int i = step + 1; i < size; i++) {
+        
+                // To sort in descending order, change > to < in this line.
+                // Select the minimum element in each loop.
+                if (array[i] < array[min_idx]) {
+                  min_idx = i;
+                }
+              }
+        
+              // put min at the correct position
+              int temp = array[step];
+              array[step] = array[min_idx];
+              array[min_idx] = temp;
+            }
+          }
     }
