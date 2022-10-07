3bb0408db4174a337fe7d68c0b8f6f2b3e2696ca
MohammedAlsabaa <mys-200@hotmail.com>
2022-10-07 19:33:51 +0300

    NEW

--- Sorter.java
+++ Sorter.java
@@ -2,12 +2,11 @@ public class Sorter{
 
     public static void main(String[] args) {
     
-    
-    
-    
         System.out.println("Hello!!!!");
-        System.out.print("We are the best");
-    
+        System.out.println("We are the best");
+        System.out.println("How to use commit");
+
+        
     }
     
     
